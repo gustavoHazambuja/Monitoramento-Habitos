c2138aef85ed875cdf9ddb5269437896b8e13d7c
Gustavo Henrique <guazambujinha@gmail.com>
2025-03-17 16:01:11 -0300

    fist commit

---
+++ Monitora-Habitos/src/main/java/Monitora_Habitos/MonitoraHabitosApplication.java
@@ -0,0 +1,13 @@
+package Monitora_Habitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoraHabitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoraHabitosApplication.class, args);
+	}
+
+}
---
+++ Monitora-Habitos/src/test/java/Monitora_Habitos/MonitoraHabitosApplicationTests.java
@@ -0,0 +1,13 @@
+package Monitora_Habitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoraHabitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
